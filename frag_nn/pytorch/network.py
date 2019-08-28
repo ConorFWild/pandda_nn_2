f1ecd644529c20699646113bbde92c326dc0f003
Oregeno <conor.wild@sky.com>
2019-08-28 11:42:18 +0100

    v6 classifier simple

--- frag_nn/pytorch/network.py
+++ frag_nn/pytorch/network.py
@@ -1206,14 +1206,14 @@ class ClassifierV6(nn.Module):
                                          )
 
         self.conv_pool_2 = nn.Sequential(nn.Conv3d(filters, filters * 2, kernel_size=3, stride=1, padding=1),
-                                         nn.BatchNorm3d(filters),
+                                         nn.BatchNorm3d(filters * 2),
                                          nn.MaxPool3d(kernel_size=2, stride=2),
                                          nn.ReLU(),
                                          nn.Dropout3d(p=0.5)
                                          )
 
         self.conv_pool_3 = nn.Sequential(nn.Conv3d(filters * 2, filters * 4, kernel_size=3, stride=1, padding=1),
-                                         nn.BatchNorm3d(filters),
+                                         nn.BatchNorm3d(filters * 4),
                                          nn.MaxPool3d(kernel_size=2, stride=2),
                                          nn.ReLU(),
                                          nn.Dropout3d(p=0.5)
