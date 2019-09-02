03889663dd238edfec21a6735ce9cebe65531b61
Oregeno <conor.wild@sky.com>
2019-09-02 12:35:56 +0100

    12 epochs

--- frag_nn/pytorch/trainscript_cuda_stfc.py
+++ frag_nn/pytorch/trainscript_cuda_stfc.py
@@ -71,7 +71,7 @@ if __name__ == "__main__":
     dataset_version = 3
     train = "gpu"
     transforms = "rottrans"
-    num_epochs = 6
+    num_epochs = 12
 
 
     state_dict_dir = "/home/zoh22914/pandda_nn_2/"
