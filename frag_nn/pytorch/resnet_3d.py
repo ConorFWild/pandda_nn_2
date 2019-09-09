8e215cc32ad0e56de4f5c5840b2a35f3ac3019eb
Oregeno <conor.wild@sky.com>
2019-09-09 12:35:02 +0100

    resnet18

--- frag_nn/pytorch/resnet_3d.py
+++ frag_nn/pytorch/resnet_3d.py
@@ -149,7 +149,7 @@ class ResNet(nn.Module):
                                        dilate=replace_stride_with_dilation[1])
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
                                        dilate=replace_stride_with_dilation[2])
-        self.avgpool = nn.AdaptiveAvgPool3d((1, 1))
+        self.avgpool = nn.AdaptiveAvgPool3d((1, 1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
